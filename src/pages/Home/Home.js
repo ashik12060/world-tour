6d7d6dbdaabac13ccb69650a1dc1251e573b4298
Ashik Mahmood <wd.ashik04@gmail.com>
2023-03-14 15:02:19 +0600

    first commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is home!</h1>
+            <button className="p-4 text-3xl bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Button</button>
+
+        </div>
+    );
+};
+
+export default Home;
